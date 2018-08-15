9c7e25711a7b9017af5053afe8da55aa671dc959
Mark Shipley <mshipley1992@hotmail.com>
2018-08-15 21:44:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist={artistName:"song"};
-function updatePlaylist(playlist,artistName,songTitle){
+function updatePlaylist(playlist, artistName,songTitle){
 playlist[artistName]=songTitle
 }
 function removeFromPlaylist(playlist, artistName){
